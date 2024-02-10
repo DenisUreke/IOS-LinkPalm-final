41cb65228364843ecaca42fb79fc9abd21bbd642
Denis Ureke <urekedenis@gmail.com>
2024-02-10 13:10:42 +0100

    Before Trying to add background Image

---
+++ LinkPalm/Views/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  LinkPalm
+//
+//  Created by Denis Ureke on 2024-02-10.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
