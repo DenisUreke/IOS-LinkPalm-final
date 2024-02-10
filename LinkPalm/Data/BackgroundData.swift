f7a8002648c671762afd72ac200ed32a96f33406
Denis Ureke <urekedenis@gmail.com>
2024-02-10 12:28:52 +0100

    Implemented Dual Color Gradient, and background modifier

---
+++ LinkPalm/Data/BackgroundData.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundDesignData.swift
+//  LinkPalm
+//
+//  Created by Denis Ureke on 2024-02-10.
+//
+
+import SwiftUI
+
+struct BackgroundDesignData {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundDesignData()
+}
